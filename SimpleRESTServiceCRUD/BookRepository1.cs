8711e8cdaac64b8bf5e27b3284e14746acdda11a
Allando <alla2634@edu.easj.dk>
2016-10-24 09:17:02 +0200

    plplplpl

---
+++ SimpleRESTServiceCRUD/BookRepository1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleRESTServiceCRUD
+{
+    public class BookRepository1 : IBook
+    {
+        public List<Book> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book AddNewBook(Book item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteABook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateABook(Book item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleRESTServiceCRUD/IBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRESTServiceCRUD
+{
+    interface IBook
+    {
+        [OperationContract]
+        List<Book> GetAllBooks();
+
+        [OperationContract]
+        Book GetBookById(int id);
+
+        [OperationContract]
+        Book AddNewBook(Book item);
+
+        [OperationContract]
+        bool DeleteABook(int id);
+
+        [OperationContract]
+        bool UpdateABook(Book item);
+    }
+}
